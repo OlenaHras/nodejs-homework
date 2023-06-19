6127171547696752e167b183282c3b778db6e58a
OlenaHras <alena.hras@gmail.com>
2023-06-19 11:35:39 +0300

    add avatars adding

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,12 @@
 const { validateBody, validateFavorite } = require("./validateBody");
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
-module.exports = { validateBody, validateFavorite, isValidId, authenticate };
+
+const upload = require("./upload");
+module.exports = {
+  validateBody,
+  validateFavorite,
+  isValidId,
+  authenticate,
+  upload,
+};
